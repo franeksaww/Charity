50668ff04b2204a4b5ac331389bb6bd1330606da
Franek Sawicki <franeksawicki@DESKTOP-BK9JHAD.localdomain>
2020-03-25 10:16:54 +0100

    ex 1 completed, models done

--- main_apk/models.py
+++ main_apk/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+TYPES = (
+    (1, 'fundacja'),
+    (2, 'organizacja pozarządowa'),
+    (3, 'zbiórka lokalna')
+)
+
+
+class CategoryModel(models.Model):
+    name = models.CharField(max_length=128)
+
+
+class InstitutionModel(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    type = models.IntegerField(choices=TYPES, default=1)
+    categories = models.ManyToManyField(CategoryModel)
+
+
+class DonationModel(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(CategoryModel)
+    institution = models.ForeignKey(InstitutionModel,on_delete=models.CASCADE)
+    address = models.CharField(max_length=256)
+    phone_number = models.IntegerField()
+    city = models.CharField(max_length=128)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, null=True, default=None, on_delete=models.CASCADE)
